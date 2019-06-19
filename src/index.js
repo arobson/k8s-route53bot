a8f1cf28761758fa4ba8eb1f3915f130f9f72dd3
Alexander Early <alex@npmjs.com>
2019-06-19 15:37:50 -0700

    chore: refactor to async/await

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const _ = require('lodash')
+require('dotenv').config()
+const util = require('util')
 const hikaru = require('@npm-wharf/hikaru')
 const Route53 = require('nice-route53')
-const Promise = require('bluebird')
 
 const router = new Route53({
   accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -10,109 +10,77 @@ const router = new Route53({
 const ZONE_NAME = process.env.AWS_ZONE_NAME
 const DOMAIN_NAMES = process.env.AWS_DOMAIN_NAMES
 
-function createRecord () {
+const upsertRecord = util.promisify(router.upsertRecord.bind(router))
+const listZones = util.promisify(router.zones.bind(router))
+const listRecords = util.promisify(router.records.bind(router))
+
+async function createRecord () {
   console.log(`connecting to kubernetes at ${process.env.K8S_URL}`)
-  return hikaru.connect()
-    .then(
-      onConnected,
-      onFailed
-    )
-}
-
-function findZone () {
-  return new Promise((resolve, reject) => {
-    router.zones((err, zones) => {
-      if (err) {
-        console.log('an error occurred trying to look up the zone', err)
-        reject(err)
-      } else {
-        const zone = _.find(zones, {name: ZONE_NAME})
-        if (zone) {
-          console.log(`  found zone '${ZONE_NAME} with id: '${zone.zoneId}'`)
-          resolve(zone.zoneId)
-        } else {
-          reject(new Error(`No zone named ${ZONE_NAME} found`))
-        }
-      }
-    })
+  await hikaru.connect().catch(err => {
+    console.log(`Could not connect to kubernetes ${process.env.K8S_URL}:\n\t${err.message}`)
+    throw err
   })
-}
 
-function findRecord (zoneId, domain) {
-  return new Promise((resolve, reject) => {
-    console.log(`looking up records in zone '${zoneId}'`)
-    router.records(zoneId, (err, records) => {
-      if (err) {
-        console.log('an error occurred looking up records in the zone', err)
-        reject(err)
-      } else {
-        console.log(`  checking ${records.length} records for '${domain}'`)
-        const pattern = domain.replace('*', '\\052')
-        const record = _.find(records, {name: pattern})
-        if (record) {
-          console.log('  matching record found')
-          resolve(record)
-        } else {
-          console.log('  no matching record found')
-          resolve(undefined)
-        }
-      }
-    })
-  })
-}
+  const zoneId = await findZone()
 
-function onConnected () {
-  return findZone()
-    .then(onZoneId)
-}
-
-function onFailed (err) {
-  console.log(`Could not connect to kubernetes ${process.env.K8S_URL}:\n\t${err.message}`)
-  throw err
-}
-
-function onRecord (zoneId, domain, record) {
-  console.log(`looking up LoadBalacners in kubernetes`)
-  return hikaru.k8s.getLoadBalancers()
-    .then(
-      lbs => {
-        if (lbs.length > 0) {
-          console.log(`    found ${lbs.length} services marked as loadbalancers`)
-          const set = lbs.reduce((acc, lb) => {
-            const ips = lb.status.loadBalancer.ingress.map(i => i.ip)
-            const id = [lb.metadata.name, lb.metadata.namespace].join('.')
-            if (ips.length) {
-              acc[id] = ips
-              acc.ips = acc.ips.concat(ips)
-            }
-            return acc
-          }, {ips: []})
-          console.log(`    with ${set.ips.length} IPs`)
-          return setupRoute(zoneId, domain, record, set)
-        } else {
-          console.log(`    no services marked as loadbalancers were found, checking again in 5 seconds`)
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              return onRecord(zoneId, domain, record)
-                .then(resolve, reject)
-            }, 5000)
-          })
-        }
-      }
-    )
-}
-
-function onZoneId (zoneId) {
   const domains = DOMAIN_NAMES.split(',').map(x => x.trim())
-  const changes = domains.map(domain => {
-    return findRecord(zoneId, domain)
-      .then(
-        record => {
-          return onRecord(zoneId, domain, record)
-        }
-      )
+  const changes = domains.map(async domain => {
+    const record = await findRecord(zoneId, domain)
+
+    const set = await findLoadBalancerIPs(zoneId, domain, record)
+
+    return setupRoute(zoneId, domain, record, set)
   })
+
   return Promise.all(changes)
+}
+
+async function findZone () {
+  const zones = await listZones()
+  const zone = zones.find(({ name }) => name === ZONE_NAME)
+  if (zone) {
+    console.log(`  found zone '${ZONE_NAME} with id: '${zone.zoneId}'`)
+    return zone.zoneId
+  } else {
+    throw new Error(`No zone named ${ZONE_NAME} found`)
+  }
+}
+
+async function findRecord (zoneId, domain) {
+  console.log(`looking up records in zone '${zoneId}'`)
+  const records = await listRecords(zoneId)
+
+  console.log(`  checking ${records.length} records for '${domain}'`)
+  const pattern = domain.replace('*', '\\052')
+  const record = records.find(({ name }) => name === pattern)
+  if (record) {
+    console.log('  matching record found')
+    return record
+  } else {
+    console.log('  no matching record found')
+  }
+}
+
+async function findLoadBalancerIPs (zoneId, domain, record) {
+  console.log(`looking up LoadBalacners in kubernetes`)
+  const lbs = await hikaru.k8s.getLoadBalancers()
+  if (lbs.length > 0) {
+    console.log(`    found ${lbs.length} services marked as loadbalancers`)
+    const set = lbs.reduce((acc, lb) => {
+      const ips = lb.status.loadBalancer.ingress.map(i => i.ip)
+      const id = [lb.metadata.name, lb.metadata.namespace].join('.')
+      if (ips.length) {
+        acc[id] = ips
+        acc.ips = acc.ips.concat(ips)
+      }
+      return acc
+    }, { ips: [] })
+    console.log(`    with ${set.ips.length} IPs`)
+  } else {
+    console.log(`    no services marked as loadbalancers were found, checking again in 5 seconds`)
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    return findLoadBalancerIPs(zoneId, domain, record)
+  }
 }
 
 function setupRoute (zoneId, domain, record, set) {
@@ -126,7 +94,7 @@ function setupRoute (zoneId, domain, record, set) {
   console.log(`loadbalancer ips:\n${JSON.stringify(set, null, 2)}`)
   if (record) {
     console.log(`checking zone record for'${domain}' for ${set.ips.length > 1 ? 'IPs' : 'IP'} ${set.ips.join(', ')}`)
-    if (_.every(set.ips.map(ip => record.values.includes(ip)))) {
+    if (set.ips.every(ip => record.values.includes(ip))) {
       console.log(`  zone record is correct`)
       return Promise.resolve(newRecord)
     } else {
@@ -137,18 +105,6 @@ function setupRoute (zoneId, domain, record, set) {
     console.log(`creating zone record for '${domain}' to ${set.ips.length > 1 ? 'IPs' : 'IP'} ${set.ips.join(', ')}`)
     return upsertRecord(newRecord)
   }
-}
-
-function upsertRecord (record) {
-  return new Promise((resolve, reject) => {
-    router.upsertRecord(record, (err, res) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  })
 }
 
 module.exports = {
